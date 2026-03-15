43e6bbf17fed3c2fbc19816294a4f72bf22bc979
bpeniston <comms@navybook.com>
2026-03-15 13:54:58 -0400

    second test

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (!empty($_SESSION['prefill'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<!-- test2 -->
+<!-- test3 -->
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SEO Headline Generator · Newsroom</title>
