f1c698534185280dc54d8f217f0448c841dc506e
Brad Peniston <comms@navybook.com>
2026-03-16 15:15:19 -0400

    clean up

--- seo-api.php
+++ seo-api.php
@@ -40,13 +40,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Methods: POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type');
-    http_response_code(204);
-    exit;
-}
-
 if (defined('BASIC_AUTH_USER')) {
     if (
         !isset($_SERVER['PHP_AUTH_USER']) ||
